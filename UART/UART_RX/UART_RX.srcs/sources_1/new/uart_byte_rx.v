b1e98e83ffb36e0d215bf875b041820ce262649c
christopher <christopherhyx@outlook.com>
2026-02-07 23:47:30 +0800

    uart_rx_init

---
+++ UART/UART_RX/UART_RX.srcs/sources_1/new/uart_byte_rx.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2026/02/07 23:37:21
+// Design Name: 
+// Module Name: uart_byte_rx
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module uart_byte_rx(
+    
+    );
+endmodule
