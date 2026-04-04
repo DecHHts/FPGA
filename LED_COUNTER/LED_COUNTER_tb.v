c67e53fbed3c0972c7145d702ee4985f2e742fa0
HE YUXUAN <christopherhyx@outlook.com>
2026-04-04 18:46:53 +0800

    finish LED_COUNTER_tb.v

---
+++ LED_COUNTER/LED_COUNTER_tb.v
@@ -0,0 +1,19 @@
+module LED_COUNTER_tb();
+    reg clk;
+    reg rst_n;
+    wire led;
+    LED_COUNTER U_LED_COUNTER(
+        .clk(clk),
+        .rst_n(rst_n),
+        .led(led)
+    );
+    initial clk = 1;
+    always #10 clk = ~clk;
+    initial begin
+        rst_n = 0;
+        #201;
+        rst_n = 1;
+        #2000_000_000;
+        $stop;
+    end
+endmodule
