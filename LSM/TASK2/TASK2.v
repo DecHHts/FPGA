e508e14caf63cfecdf408ba76e56ea3a192bad9f
HE YUXUAN <christopherhyx@outlook.com>
2026-04-06 14:50:28 +0800

    fix TASK2 bugs

--- LSM/TASK2/TASK2.v
+++ LSM/TASK2/TASK2.v
@@ -1,4 +1,4 @@
-module TASK1(
+module TASK2(
     clk,
     rst_n,
     led
@@ -13,7 +13,7 @@ module TASK1(
         if(!rst_n)
             clk_counter <= 0;
         else
-            if(clk_counter == 23'd12_500_000 - 1)
+            if(clk_counter == 24'd12_500_000 - 1)
                 clk_counter <= 0;
             else
                 clk_counter <= clk_counter + 1'b1;
@@ -22,24 +22,27 @@ module TASK1(
     always@(posedge clk or negedge rst_n)begin
         if(!rst_n)
             state_counter <= 0;
-        else if(state_counter == 10)
-            state_counter <= 0;
-        else
-            if(clk_counter == 23'd12_500_000 - 1)
-                state_counter <= state_counter + 1;
+        else begin
+            if(clk_counter == (24'd12_500_000 - 1))begin
+                if(state_counter == 9)
+                    state_counter <= 0;
+                else
+                    state_counter <= state_counter + 1;
+            end
+        end
     end
     always@(*)begin
         case(state_counter)
-            0:led = 1;
-            1:led = 0;
-            2:led = 0;
-            3:led = 1;
-            4:led = 1;
-            5:led = 1;
-            6:led = 0;
-            7:led = 0;
-            8:led = 0;
-            9:led = 0;
+            0:led <= 1;
+            1:led <= 0;
+            2:led <= 0;
+            3:led <= 1;
+            4:led <= 1;
+            5:led <= 1;
+            6:led <= 0;
+            7:led <= 0;
+            8:led <= 0;
+            9:led <= 0;
         endcase
     end
 endmodule
